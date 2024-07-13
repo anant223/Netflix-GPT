f17e67fe0569838fd47bd9cc359ebaae4fa7639a
Anant <p.anant223@gmail.com>
2024-07-13 08:37:45 +0530

    styling

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -3,22 +3,21 @@ import Btn from './Btn'
 
 const VideoDetails = ({ desc, title }) => {
   return (
-    <div className=" relative sm:pl-[60px] pl-[10px] sm:pr-[0.9rem] lg:mr-[28rem] sm:mr-[18rem] mr-[8rem] sm:mt-0 mt-[12px]">
-      <div className=" block text-justify relative">
-        <h2 className=" font-bold sm:text-xl text-xs">{title}</h2>
-        <p className="sm:mt-[20px] mt-[10px] sm:line-clamp-3 line-clamp-2 sm:text-lg text-[8.9px]">{desc}</p>
+    <div className="relative sm:pl-[60px] pl-[10px] sm:pr-[0.9rem] lg:w-[calc(100%-28rem)] sm:w-[calc(100%-18rem)] w-[calc(100%-8rem)] sm:mt-0 mt-[12px]">
+      <div className="block text-justify relative">
+        <h2 className="font-bold sm:text-xl text-xs">{title}</h2>
+        <p className="sm:mt-[20px] mt-[10px] sm:line-clamp-3 line-clamp-2 sm:text-lg text-[8.9px]">
+          {desc}
+        </p>
       </div>
       <div className="sm:mt-[20px] mt-[10px] flex sm:gap-5 gap-3 text-black">
         <div>
-          <Btn
-            name="▷ Play"
-            className="btn sm:btn-md btn-xs"
-          />
+          <Btn name="▷ Play" className="btn sm:btn-md btn-xs" />
         </div>
         <div>
           <Btn
             name="ⓘ More Info"
-            className=" sm:btn-outline btn-outline btn sm:btn-md btn-xs  border-white sm:text-white text-white"
+            className="sm:btn-outline btn-outline btn sm:btn-md btn-xs border-white sm:text-white text-white"
           />
         </div>
       </div>
